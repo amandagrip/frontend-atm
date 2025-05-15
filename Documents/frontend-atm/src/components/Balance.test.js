dd66b8a9787a44f9d95dd140789baac3b840e961
Amanda Grip <amanda.grip@stud.sti.se>
2025-05-15 22:33:26 +0200

    Ändring av testtext i Balance.test.js

--- Documents/frontend-atm/src/components/Balance.test.js
+++ Documents/frontend-atm/src/components/Balance.test.js
@@ -1,12 +1,12 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import Balance from "./Balance";
 
-test("Visa saldo samt sínsättning av pengar.", () => {
+test("Visa saldo samt insättning av pengar.", () => {
     render(<Balance />);
 
     expect(screen.getByText("Ditt saldo: 0 kr")).toBeInTheDocument();
 
-    fireEvent.click(screen.getByText("Kolla saldo."));
+    fireEvent.click(screen.getByText("Kolla saldo"));
 
     expect(screen.getByText("Ditt saldo: 1500 kr")).toBeInTheDocument();
 
